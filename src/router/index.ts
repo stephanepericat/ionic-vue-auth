edd0082b6e0cafd00c37aabc8a91efe442f90368
Stephane P. Pericat <shortwavaudio@gmail.com>
2022-07-18 16:59:26 -0500

    add auth guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,13 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw, Router } from 'vue-router';
-// import { createAuthGuard } from "@auth0/auth0-vue";
+import { createAuthGuard } from "@auth0/auth0-vue";
 import { App } from 'vue';
 
 import HomePage from '../views/HomePage.vue';
 import ProfilePage from '../views/ProfilePage.vue';
 
-// const routes: Array<RouteRecordRaw> = [
-//   {
-//     path: '/',
-//     redirect: '/home'
-//   },
-//   {
-//     path: '/home',
-//     name: 'Home',
-//     component: HomePage
-//   },
-//   {
-//     path: '/profile',
-//     name: 'Profile',
-//     component: ProfilePage,
-//     beforeEnter: createAuthGuard(),
-//   }
-// ]
-
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes
-// })
-
-// export default router
 export default (app: App) : Router => {
   const routes: Array<RouteRecordRaw> = [
-    // {
-    //   path: '/',
-    //   redirect: '/home'
-    // },
     {
       path: '/',
       name: 'Home',
@@ -45,10 +17,7 @@ export default (app: App) : Router => {
       path: '/profile',
       name: 'Profile',
       component: ProfilePage,
-      // beforeEnter: createAuthGuard(app),
-      beforeEnter: (to, from, next) => {
-        next();
-      }
+      beforeEnter: createAuthGuard(app),
     }
   ]
   
